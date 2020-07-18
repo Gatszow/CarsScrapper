b9808a4620c3e2e0b6adde1ac28a87e00bdc0d15
Gatszow <hrysins@gmail.com>
2020-07-18 23:54:46 +0200

    Updating function

--- scrapper.py
+++ scrapper.py
@@ -3,10 +3,7 @@ from exceptions import WrongThingToGetError
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException as NSEE, \
-    ElementClickInterceptedException as ECIE, \
-    TimeoutException as TE, \
-    InvalidSessionIdException as ISIE
+from selenium.common.exceptions import NoSuchElementException as NSEE, ElementNotInteractableException as ENIE
 
 
 def is_negotiable(string: str):
@@ -14,11 +11,6 @@ def is_negotiable(string: str):
         string = 'True'
     else:
         string = 'False'
-    return string
-
-
-def convert_to_str(input_seq):
-    string = ' '.join(input_seq)
     return string
 
 
@@ -47,12 +39,13 @@ class CarsScrapper(object):
                    '%5Bfilter_enum_damaged%5D=0&search' \
                    '%5Bfilter_enum_no_accident%5D=1&search' \
                    '%5Border%5D=created_at%3Adesc&search%5Bbrand_program_id%5D' \
-                   '%5B0%5D=&search%5Bcountry%5D=&view=list'
+                   '%5B0%5D=&search%5Bcountry%5D=&view=list&page=209'
 
         self.driver = webdriver.Firefox()
         self.driver.get(self.url)
         self.isclosed = False
         self.list_of_tuples = []
+        self.count = 1
 
         self.makes = []
         self.excluded_makes = ['Alfa Romeo', 'Aston Martin', 'De Lorean', 'Land Rover', 'DS Automobiles']
@@ -69,28 +62,27 @@ class CarsScrapper(object):
 
     def get_products_make_and_model(self, title_class_name: str):
         titles = self.driver.find_elements_by_class_name(title_class_name)
+
         for title in titles:
             if self.excluded_makes[0] in title.text or self.excluded_makes[1] in title.text or self.excluded_makes[2] \
                     in title.text or self.excluded_makes[3] in title.text or self.excluded_makes[4] in title.text:
 
-                self.models.append(convert_to_str(title.text.split()[2:]))
+                self.models.append(' '.join((title.text.split()[2:])))
                 temp_makes = title.text.split()[:2]
                 make = ' '.join(temp_makes)
                 self.makes.append(make)
                 temp_makes.clear()
 
             else:
-                self.models.append(convert_to_str(title.text.split()[1:]))
+                self.models.append(' '.join((title.text.split()[1:])))
                 self.makes.append(title.text.split()[0])
 
-        len_of_elements = len(self.models)
-
-        return self.makes, self.models, len_of_elements
+        return self.makes, self.models
 
     def get_products(self, thing_to_get, counter):
         try:
             if thing_to_get == 'mileage':
-                for i in range(1, counter+1):
+                for i in range(1, counter + 1):
                     try:
                         mileage = self.driver.find_element_by_xpath(
                             f'/html/body/div[4]/div[2]/section/div[2]/div[1]/div/div[1]/div[5]/article[{i}'
@@ -166,6 +158,7 @@ class CarsScrapper(object):
                 value, currency = get_price_and_currency(price.text)
                 self.prices.append(value)
                 self.currencies.append(currency)
+
             except NSEE:
                 price = self.driver.find_element_by_xpath(
                     f'/html/body/div[5]/div[2]/section/div[2]/div[1]/div/div[1]/div[5]/article[{i}'
@@ -173,19 +166,24 @@ class CarsScrapper(object):
                 value, currency = get_price_and_currency(price.text)
                 self.prices.append(value)
                 self.currencies.append(currency)
+
             else:
-                negotiable = "Failed to get"
-                self.negotiable.append(is_negotiable(negotiable))
+                price = "Failed to get"
+                self.prices.append(price)
+                self.currencies.append(price)
 
             try:
-                negotiable = self.driver.find_element_by_xpath(f'/html/body/div[4]/div[2]/section/div[2]/div[1]/div/d'
-                                                               f'iv[1]/div[5]/article[{i}]/div[2]/div[2]/div/span').text
+                negotiable = self.driver.find_element_by_xpath(
+                    f'/html/body/div[4]/div[2]/section/div[2]/div[1]/div/d'
+                    f'iv[1]/div[5]/article[{i}]/div[2]/div[2]/div/span').text
                 self.negotiable.append(is_negotiable(negotiable))
+
             except NSEE:
                 negotiable = self.driver.find_element_by_xpath(
                     f'/html/body/div[5]/div[2]/section/div[2]/div[1]/div/div[1]'
                     f'/div[5]/article[{i}]/div[2]/div[2]/div/span').text
                 self.negotiable.append(is_negotiable(negotiable))
+
             else:
                 negotiable = "Failed to get"
                 self.negotiable.append(is_negotiable(negotiable))
@@ -193,58 +191,63 @@ class CarsScrapper(object):
         return self.prices, self.currencies, self.negotiable
 
     def search(self):
-        for x in range(10):
+        while True:
             if self.isclosed:
                 break
+
             else:
-                makes, models, end_of_counter = self.get_products_make_and_model('offer-title__link')
-                mileages = self.get_products('mileage', end_of_counter)
-                years = self.get_products('year', end_of_counter)
-                fuels = self.get_products('fuel', end_of_counter)
-                engine_sizes = self.get_products('engine_size', end_of_counter)
-                urls = self.get_products('url', end_of_counter)
-                prices, currencies, negotiable = self.get_products_price_and_currency(end_of_counter)
-                for i in range(end_of_counter):
+                number_of_articles = len(self.driver.find_elements_by_tag_name('article'))
+
+                makes, models = self.get_products_make_and_model('offer-title__link')
+                mileages = self.get_products('mileage', number_of_articles)
+                years = self.get_products('year', number_of_articles)
+                fuels = self.get_products('fuel', number_of_articles)
+                engine_sizes = self.get_products('engine_size', number_of_articles)
+                urls = self.get_products('url', number_of_articles)
+                prices, currencies, negotiable = self.get_products_price_and_currency(number_of_articles)
+
+                for i in range(number_of_articles):
                     temp = (makes[i], models[i], mileages[i], years[i], fuels[i],
                             engine_sizes[i], urls[i], prices[i], currencies[i], negotiable[i])
-                    print(temp)
                     self.list_of_tuples.append(temp)
                     del temp
 
                 makes.clear(), models.clear(), mileages.clear(), years.clear(), fuels.clear(), engine_sizes.clear()
                 urls.clear(), prices.clear(), currencies.clear(), negotiable.clear()
+
                 self.next_page()
 
         return self.list_of_tuples
 
     def next_page(self):
         try:
-            nexts = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located(
-                    (By.XPATH, "/html/body/div[4]/div[2]/section/div[2]/div[2]/ul/li[7]/a"))
-            )
-            nexts.click()
-
-        except ECIE:
             interupting_element = self.driver.find_element_by_xpath('/html/body/div[4]/div[15]/div/div/a')
             interupting_element.click()
+
+        except ENIE:
+            pass
+
+        li_index = len(self.driver.find_element_by_xpath('/html/body/div[4]/div[2]/section/div[2]/div[2]/ul')
+                       .find_elements_by_tag_name('li'))
+
+        if li_index == 7 and self.count == 2:
+            self.isclosed = True
+            self.driver.close()
+
+        elif self.count == 1:
             nexts = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "/html/body/div[4]/div[2]/section/div[2]/div[2]/ul/li[7]/a"))
+                    (By.XPATH, f"/html/body/div[4]/div[2]/section/div[2]/div[2]/ul/li[{li_index}]/a"))
             )
             nexts.click()
+            self.count = 2
 
-        except TE:
-            #self.driver.close()
-            #self.isclosed = True
-            print('\n TimeOut \n')
-
-        except ISIE as e:
-            print(e)
         else:
-            print('Else statement')
+            nexts = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(
+                (By.XPATH, f'/html/body/div[4]/div[2]/section/div[2]/div[2]/ul/li[{li_index}]/a'))
+            )
+            nexts.click()
 
 
 if __name__ == '__main__':
     CarsScrapper().search()
-'''Poprawić next element i search, reszta jak narazie w porządku'''
